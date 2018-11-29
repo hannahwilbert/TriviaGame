928cb524f4af583e65ec5e249c4ba7ee86ea918a
hannahwilbert <hannahclaire230@yahoo.com>
2018-11-29 15:12:03 -0800

    fixed js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -31,24 +31,20 @@
     var answers = ["b","a","d","b","d"];
 
     // check answers
+    for(i = 1; i <= total; i++){
+        if(eval('q' + i) === answers[i - 1]) {
+            score++;
+        }
+     }
 
-    if(q1 === answers[0]) {
-        score++;
-    }
-    if(q2 === answers[1]) {
-        score++;
-    }
-    if(q3 === answers[2]) {
-        score++;
-    }
-    if(q4 === answers[3]) {
-        score++;
-    }
-    if(q5 === answers[4]) {
-        score++;
-    }
+
+    //  display results
+    var results = document.getElementById('results');
+    results.innerHTML = '<h3>You Scored <span>' + score+'</span> out of <span>'+total+'</span></h3>';
 alert('You scored '+ score + ' out of ' + total)
 console.log(alert)
+
+return false;
 
   
  }
