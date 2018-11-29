d42e3ba22f1a235530d266d3646bfb551c301279
hannahwilbert <hannahclaire230@yahoo.com>
2018-11-29 14:16:19 -0800

    html and started js

---
+++ assets/javascript/app.js
@@ -0,0 +1,54 @@
+// PSEUDO CODING
+// multiple choice questions
+// timed for 30 seconds
+// when buttn pressed or timed out, scores display
+
+ function submitAnswers() {
+     var total = 5;
+     var score = 0;
+
+    //  get user input
+
+    var q1 =  document.forms["quizForm"] ["q1"].value;
+    var q2 =  document.forms["quizForm"] ["q2"].value;
+    var q3 =  document.forms["quizForm"] ["q3"].value;
+    var q4 =  document.forms["quizForm"] ["q4"].value;
+    var q5 =  document.forms["quizForm"] ["q5"].value;
+
+  
+
+    for(i = 1; i <= total; i++){
+        if(eval('q' + i) === null || eval('q' + i) === '') {
+            alert('You missed question ' + i);
+            return false;
+            console.log(alert)
+        }
+        
+    }
+
+    // set correct answers
+
+    var answers = ["b","a","d","b","d"];
+
+    // check answers
+
+    if(q1 === answers[0]) {
+        score++;
+    }
+    if(q2 === answers[1]) {
+        score++;
+    }
+    if(q3 === answers[2]) {
+        score++;
+    }
+    if(q4 === answers[3]) {
+        score++;
+    }
+    if(q5 === answers[4]) {
+        score++;
+    }
+alert('You scored '+ score + ' out of ' + total)
+console.log(alert)
+
+  
+ }
